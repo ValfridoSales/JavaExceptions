a625bdd9ee6736583207fc5a8e85056f248d3967
Valfrido Sales <valfridosales@hotmail.com>
2025-07-25 16:16:38 -0400

    First Solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,18 +21,34 @@ public class Program {
         System.out.println("Check-out date (dd/MM/yyyy)");
         Date checkOut = sdf.parse(sc.next());
 
-        Reservation reservation = new Reservation(number, checkIn, checkOut);
-        System.out.println("Reservation: " + reservation);
-        System.out.println();
 
-        System.out.println("Enter data to update the reservation");
-        System.out.println("Check-in date (dd/MM/yyyy)");
-        Date checkIn = sdf.parse(sc.next());
-        System.out.println("Check-out date (dd/MM/yyyy)");
-        Date checkOut = sdf.parse(sc.next());
-        reservation.updateDates(checkIn, checkOut);
-        System.out.println("Reservation: " + reservation);
+        if (!checkOut.after(checkIn)){
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }
+        else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
 
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation");
+            System.out.println("Check-in date (dd/MM/yyyy)");
+            checkIn = sdf.parse(sc.next());
+            System.out.println("Check-out date (dd/MM/yyyy)");
+            checkOut = sdf.parse(sc.next());
+
+            Date now = new Date();
+            if(checkIn.before(now) || checkOut.before(now)){
+                System.out.println("Erro in reservation: Reservation dates for update must be future dates");
+            }
+            else if (!checkOut.after(checkIn)){
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            }else {
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println("Reservation: " + reservation);
+            }
+
+        }
 
         sc.close();
     }
